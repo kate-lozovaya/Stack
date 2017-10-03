182014ed214bb4dacda8ec41264c4a955bab28c4
kate-lozovaya <katekalozovaya@gmail.com>
2017-10-03 07:22:03 +0300

    Update test1.cpp

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,18 +1,21 @@
 #include <catch.hpp>
 
-#include "palindrome.hpp"
+#include "stack.hpp"
 
-TEST_CASE("121 -> 121", "") {
-    auto is_palindrome = palindrome(L"121"); 
-    REQUIRE(is_palindrome);
+TEST_CASE("push") 
+{
+    stack<int> Stack;
+    Stack.push(5);
+    Stack.pash(7);
+    REQUIRE(Stack.count() == 2);
 }
 
-TEST_CASE("яна и аня", "") {
-    auto is_palindrome = palindrome(L"яна и аня", std::locale("ru_RU.UTF-8"));
-    REQUIRE(is_palindrome);
-}
-
-TEST_CASE("empty string", "") {
-    auto is_palindrome = palindrome(L"");
-    REQUIRE(is_palindrome);
+TEST_CASE("pop") 
+{
+    stack<int> Stack;
+    Stack.push(5);
+    Stack.pash(7);
+    Stack.push(9);
+    Stack.pop();
+    REQUIRE(Stack.count() == 2);
 }
